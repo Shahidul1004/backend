4cfb0b1477b47b4da1c662802b3714f8070de258
Shahidul Islam <sonysust@gmail.com>
2023-08-24 13:00:53 +0600

    ts worker

--- src/controllers/worker.js
+++ src/controllers/worker.js
@@ -1,17 +1,17 @@
-// const path = require("path");
-// const { workerData } = require("worker_threads");
-// require("ts-node").register();
-// require(path.resolve(__dirname, workerData.path));
+const path = require("path");
+const { workerData } = require("worker_threads");
+require("ts-node").register();
+require(path.resolve(__dirname, workerData.path));
 
-const {
-  Worker,
-  isMainThread,
-  parentPort,
-  workerData,
-} = require("worker_threads");
+// const {
+//   Worker,
+//   isMainThread,
+//   parentPort,
+//   workerData,
+// } = require("worker_threads");
 
-const delayedFetch = (delay) => {
-  const timerObj = setTimeout(() => {}, delay);
-};
+// const delayedFetch = (delay) => {
+//   const timerObj = setTimeout(() => {}, delay);
+// };
 
-parentPort?.postMessage(delayedFetch(workerData.delay));
+// parentPort?.postMessage(delayedFetch(workerData.delay));
